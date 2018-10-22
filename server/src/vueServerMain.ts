c93eb35634dc9557a17c5188e921523c309f5402
Brandon Sanderson <bas.bsanders@gmail.com>
2018-10-22 14:31:29 -0700

    Partially working support for one vue folder in multi-root

--- server/src/vueServerMain.ts
+++ server/src/vueServerMain.ts
@@ -2,9 +2,13 @@ import {
   createConnection,
   InitializeParams,
   InitializeResult,
-  TextDocumentSyncKind
+  TextDocumentSyncKind,
+  WorkspaceFolder
 } from 'vscode-languageserver';
+import Uri from 'vscode-uri';
 import { VLS } from './services/vls';
+import * as fs from 'fs';
+import { promisify } from 'util';
 
 // Create a connection for the server
 const connection =
@@ -17,19 +21,62 @@ console.error = connection.console.error.bind(connection.console);
 
 // After the server has started the client sends an initilize request. The server receives
 // in the passed params the rootPath of the workspace plus the client capabilites
-connection.onInitialize((params: InitializeParams): InitializeResult => {
+connection.onInitialize((params: InitializeParams): Thenable<InitializeResult> => {
   const initializationOptions = params.initializationOptions;
 
-  const workspacePath = params.rootPath;
-  if (!workspacePath) {
+  if (params.workspaceFolders) {
+    const promises = params.workspaceFolders!.map(wf => tryFolder(wf, initializationOptions));
+    let rootPromise = promises[0];
+    for (const p of promises.slice(1)) {
+        rootPromise = rootPromise.then((v) => v || p);
+    }
+    return rootPromise.then((v) => Promise.resolve(v || {
+        capabilities: {}
+    }));
+  }
+  return Promise.resolve(initForPath(params.rootPath, initializationOptions));
+});
+
+async function tryFolder(folder: WorkspaceFolder, initOptions: any): Promise<InitializeResult | null> {
+    const uri = Uri.parse(folder.uri);
+    if (uri.scheme !== 'file') {
+        return null;
+    } else {
+        const path = uri.fsPath;
+        const fullpath = path + '/package.json';
+        let data = null;
+        try {
+            data = await promisify(fs.readFile)(fullpath, 'utf8');
+        } catch (e) {
+            console.log('No package.json for a folder. Error: ' + e);
+            //ignore except for log, probably just not a node package
+            //logging is good for user debugging purposes
+        }
+        if (data) {
+            try {
+                const json = JSON.parse(data);
+                if ('vue' in json.dependencies) {
+                    return initForPath(path, initOptions);
+                }
+            } catch (e) {
+                console.error('Bad json in ' + path);
+            }
+        }
+
+        return null;
+    }
+
+}
+
+function initForPath(path: string | null | undefined, initializationOptions: any): InitializeResult {
+  if (!path) {
     console.error('No workspace path found. Vetur initialization failed');
     return {
       capabilities: {}
     };
   }
-
-  console.log('Vetur initialized');
-  const vls = new VLS(workspacePath, connection);
+  console.log('Vetur initialized using ' + path);
+  const vls = new VLS(path, connection);
 
   if (initializationOptions) {
     vls.configure(initializationOptions.config);
@@ -52,6 +99,7 @@ connection.onInitialize((params: InitializeParams): InitializeResult => {
       colorProvider: true
     }
   };
-});
+
+}
 
 connection.listen();
